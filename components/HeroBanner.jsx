01a576658b8a0c01419b01932e5360552a2e4192
mickey-40 <mickeyarnold53@yahoo.com>
2023-03-27 11:34:20 -0500

    hooked up sanity

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,24 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
+import { urlFor } from '../lib/client'
 
-const HeroBanner = () => {
+const HeroBanner = ({HeroBanner}) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>
-          Small text
-        </p>
-        <h3>
-          Mid text
-        </h3>
-        <img src='' alt='motorcycle helmet'  className='hero-banner-image'/>
+        {console.log(HeroBanner)}
+        <p className='beats-solo'>{HeroBanner.smallText}</p>
+        <h3>{HeroBanner.midText}</h3>
+        <h3>{HeroBanner.largeText1}</h3>
+        <img src={urlFor(HeroBanner.image)} alt='motorcycle helmet'  className='hero-banner-image'/>
         <div>
-          <Link href='/product/ID'>
-            <button type='button'>Button Text</button>
+          <Link href={`/product/${HeroBanner.product}`}>
+            <button type='button'>{HeroBanner.buttonText}</button>
           </Link>
+          
           <div className='desc'>
             <h5>Description</h5>
-            <>Description</>
+            <>{HeroBanner.desc}</>
           </div>
         </div>
       </div>
